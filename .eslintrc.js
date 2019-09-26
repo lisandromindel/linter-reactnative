34d3e74a59eb2cd40c5ec5348dbe145104d18653
Lisandro Mindel <lisandro.mindel@gmail.com>
2019-09-26 14:37:22 +0200

    Moves linter rules to module export

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
-module.exports = {
-  'extends': 'airbnb',
-  'parser': 'babel-eslint',
-  'env': {
-    'jest': true,
-  },
-  'rules': {
-    'no-use-before-define': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/prop-types': 'off',
-    'comma-dangle': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'off',
-    'react/no-did-update-set-state': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'no-console': 'off'
-  },
-  'globals': {
-    "fetch": false
-  }
+{
+  'extends': '@lisandromindel/linter-reactnative'
 }
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-exports.tester = function() {
-  console.log("This is a message from the demo package");
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'react/no-did-update-set-state': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-console': 'off'
+  },
+  'globals': {
+    "fetch": false
+  }
 }
